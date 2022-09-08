9958a94c0431016674d1b07785ba30a46802c8d4
20P31A04D4 <102217872+20P31A04D4@users.noreply.github.com>
2022-09-08 15:56:43 +0530

    import java.util.Scanner; class Patterns4 {   public static void main(String args[])    {      Scanner sc=new Scanner(System.in);       int r;       r=sc.nextInt();       for(int i=1;i<=r;i++)        {          for(int j=i;j<=r;j++)           {             System.out.print((char)(65+r-i)+" ");           }          System.out.println();        }    }  }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-class Patterns2
+class Patterns4
 {
   public static void main(String args[])
    {
      Scanner sc=new Scanner(System.in);
-      int r,c;
+      int r;
       r=sc.nextInt();
       for(int i=1;i<=r;i++)
        { 
-         for(int j=1;j<=i;j++)
+         for(int j=i;j<=r;j++)
           {
-            System.out.print(j);
+            System.out.print((char)(65+r-i)+" ");
           }
          System.out.println();
        }

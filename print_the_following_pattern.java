67b1084d5e5f652c0a82b7441d429a75464d67bb
20P31A04D4 <102217872+20P31A04D4@users.noreply.github.com>
2022-09-09 20:20:14 +0530

    import java.util.Scanner; class Pattern3 {
    	public static void main(String[] args)
    	{
    		Scanner sc=new Scanner(System.in);
    		int r,c;
    		r=sc.nextInt();
    		for(int i=1;i<=r;i++)			
    		{
    			for(int j=1;j<=r;j++)	
    			{
    			    if(j==i)
    				System.out.print("0");
    				else
    				 System.out.print("x");
    			}
    			System.out.println();
    		}
    	}
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
-class Patterns2
-{
-  public static void main(String args[])
-   {
-     Scanner sc=new Scanner(System.in);
-      int r;
-      r=sc.nextInt();
-      for(int i=1;i<=r;i++)
-       { 
-         for(int j=r;j>=1;j--)
-          {
-            System.out.print(j+" ");
-          }
-         System.out.println();
-       }
-   }
- }
+class Pattern3 {
+
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int r,c;
+		r=sc.nextInt();
+		for(int i=1;i<=r;i++)			
+		{
+			for(int j=1;j<=r;j++)	
+			{
+			    if(j==i)
+				System.out.print("0");
+				else
+				 System.out.print("x");
+			}
+			System.out.println();
+		}
+	}
+}

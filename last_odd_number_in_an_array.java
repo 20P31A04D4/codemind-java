b959bbfa182e789a98863388311d4d0b66fbdb86
20P31A04D4 <102217872+20P31A04D4@users.noreply.github.com>
2022-09-18 20:38:28 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,c=0;         a=sc.nextInt();         int b[]=new int[a];         for(int i=0;i<a;i++)         {             b[i]=sc.nextInt();         }         for(int i=0;i<a;i++)         {            if(b[i]%2!=0)            {                c=b[i];            }         }         System.out.print(c);     } }

---
+++ last_odd_number_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,c=0;
+        a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            b[i]=sc.nextInt();
+        }
+        for(int i=0;i<a;i++)
+        {
+           if(b[i]%2!=0)
+           {
+               c=b[i];
+           }
+        }
+        System.out.print(c);
+    }
+}
